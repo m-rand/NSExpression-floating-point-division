18686cdda2a0dd48ca153e1e8802a993724e74cc
m-rand <79078529+m-rand@users.noreply.github.com>
2021-03-31 13:01:55 +0200

    Update nsexpression-fp-division.swift
    Fix for expressions in the format "xx/yy", where yy is a float number. Now correctly ignores this occurrence.

--- nsexpression-fp-division.swift
+++ nsexpression-fp-division.swift
@@ -1,6 +1,6 @@
 
 func fixDivision(text: String) -> String {
-    let pattern = "\\d+\\/\\d+"
+    let pattern = "\\d+\\/\\d+(?![.])"
     let regex = try! NSRegularExpression(pattern: pattern)
     let range = NSRange(location: 0, length: text.utf16.count)
     _ = regex.matches(in: text, options: [], range: range).map {
